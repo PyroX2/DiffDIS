05f4b9f6cdfbe0558975be1bfae0593378b1ca11
Jakub Wilk <kubawilk63@gmail.com>
2026-04-30 20:54:34 +0200

    Change paths in config.py

--- utils/config.py
+++ utils/config.py
@@ -1,10 +1,10 @@
 import os
 
-diste1 = '/home/exp/Documents/1st_star/YQ/DIS-main/DIS5K/DIS-TE1/'
-diste2 = '/home/exp/Documents/1st_star/YQ/DIS-main/DIS5K/DIS-TE2/'
-diste3 = '/home/exp/Documents/1st_star/YQ/DIS-main/DIS5K/DIS-TE3/'
-diste4 = '/home/exp/Documents/1st_star/YQ/DIS-main/DIS5K/DIS-TE4/'
-disvd = '/home/exp/Documents/1st_star/YQ/DIS-main/DIS5K/DIS-VD/'
+diste1 = 'DIS5K/DIS-TE1/'
+diste2 = 'DIS5K/DIS-TE2/'
+diste3 = 'DIS5K/DIS-TE3/'
+diste4 = 'DIS5K/DIS-TE4/'
+disvd = 'DIS5K/DIS-VD/'
 
 diste1 = os.path.join(diste1)
 diste2 = os.path.join(diste2)
