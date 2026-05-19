c8a9e8dff3d9955f5626a17a8a47e2312675532d
Jakub Wilk <kubawilk63@gmail.com>
2026-05-19 16:44:27 +0200

    Update run_train.sh

--- scripts/run_train.sh
+++ scripts/run_train.sh
@@ -2,9 +2,9 @@
 export CUDA_VISIBLE_DEVICES="0"
 
 python ../run_train.py \
---batchsize 4 \
---trainsize 1024 \
+--batchsize 2 \
+--trainsize 512 \
 --decay_rate 0.95 \
 --decay_epoch 30 \
---pretrained_model_name_or_path "/path/to/sd-turbo/" \
---dataset_path "/path/to/DIS5K/" \
+--pretrained_model_name_or_path "thu-ml/unidiffuser-v1" \
+--dataset_path "/path/to/DIS5K/"
